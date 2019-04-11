67b5d40dce74e1534a34799960729f70d7879682
Nikolaos Alachiotis <n.alachiotis@gmail.com>
2019-04-11 23:24:08 +0300

    first commit

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+/*  
+ *  sw_par
+ *
+ *  Copyright April 2019 by Nikolaos Alachiotis
+ *
+ *  This program is free software; you may redistribute it and/or modify its
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
+ *
+ *  For any other enquiries send an email to
+ *  Nikolaos Alachiotis (n.alachiotis@gmail.com)
+ *  
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
